145f4efe2854ab31bad0696672e7d89bca130bd3
keenanjude <60664206+keenanjude@users.noreply.github.com>
2021-10-04 15:33:18 -0400

    Updated for accurate readings

--- test/cms.py
+++ test/cms.py
@@ -14,40 +14,65 @@ if test_mode == True:
     engine = "1.6L B16B"
     transmission = "5-Speed Manual"
     chassis = "EK"
-else:
-    vin = input()
-    make = input()
-    model = input()
-    year = raw_input()
-    m_trim = input()
-    colour = input()
-    engine = input()
-    transmission = input()
-    chassis = input()
 
-print("Please enter the VIN")
-vin; #enter vin number here
+
+if test_mode == True:
+    vin
+else:
+    print("Please enter the VIN")
+    vin = input(); #enter vin number here
 
 path = vin+"/"
 
 if not os.path.exists(path):
     os.makedirs(path)
+    print("VIN not found in system, please input details about the vehicle")
     print("Please enter the Manufacturer of the Vehicle")
-    make
+    if test_mode == True:
+        make
+    else:
+        make = input()
     print("Please enter the Model")
-    model
+    if test_mode == True:
+        model
+    else:
+        model = input()
+
     print("Please enter the Year of Manufacture")
-    year
+    if test_mode == True:
+        year
+    else:
+        year = input()
+
     print("Please enter the Trim level")
-    m_trim
+    if test_mode == True:
+        m_trim
+    else:
+        m_trim = input()
+
     print("Please enter the paint code")
-    colour
+    if test_mode == True:
+        colour
+    else:
+        colour = input()
+
     print("Please enter the Engine specifications")
-    engine
+    if test_mode == True:
+        engine
+    else:
+        engine = input()
+
     print("Please enter the Transmission type of the Vehicle")
-    transmission
+    if test_mode == True:
+        transmission
+    else:
+        transmission = input()
+
     print("Please enter the Chassis designation of the Vehicle")
-    chassis #chassis code
+    if test_mode == True:
+        chassis
+    else:
+        chassis = input() #chassis code
     #head_info = {"VIN":vin, "Manufacturer":model, "Model": model, "Year": year, "Trim": m_trim, "Colour Code": colour, "Engine": engine, "Transmission": transmission, "Chassis Code": chassis}
     head_info = [vin, make, model, year, m_trim, colour, engine, transmission, chassis]
     head_info2 = ["VIN", "Manufacturer", "Model", "Year", "Trim", "Colour Code", "Engine", "Transmission", "Chassis"]
@@ -57,13 +82,15 @@ if not os.path.exists(path):
         f.write(i + "," + head_info[n] + "\n")
         n += 1
         print(n)
-    headder = ["TIMESTAMP","TIME_WORK_DONE","TYPE_OF_WORK_DONE","WORK DONE DESC", "LOCATION INFO"]
+    headder = ["TIMESTAMP,","TIME_WORK_DONE,","TYPE_OF_WORK_DONE,","WORK_DONE_DESC,", "LOCATION_INFO","\n"]
     f.writelines(headder)
 
 else:
+    print("VIN Found, Please wait while maintainance records are being accessed")
     f = open((path+vin+".csv"), 'a')
 
 
+f.close()
 
 
 
