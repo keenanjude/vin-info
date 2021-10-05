c937a0c0f0f4f79d65f3f7ef3acf8a050bf9fee9
keenanjude <60664206+keenanjude@users.noreply.github.com>
2021-10-04 20:06:52 -0400

    Update cms.py

--- test/cms.py
+++ test/cms.py
@@ -1,8 +1,12 @@
+#important
+##The first 9 lines of the CSV file contain information about the vehicle.
+##The 10th line contains the table headder, and data about service begins on line 11. 
+
 import csv
 import os
 
 
-test_mode = True
+test_mode = True #MUST BE TRUE OR FALSE
 
 if test_mode == True:
     vin = "123EK456"
@@ -18,6 +22,7 @@ if test_mode == True:
 
 if test_mode == True:
     vin
+    print("TEST MODE")
 else:
     print("Please enter the VIN")
     vin = input(); #enter vin number here
@@ -25,52 +30,61 @@ else:
 path = vin+"/"
 
 if not os.path.exists(path):
+    print("The entered VIN could not be found in the system, would you like to create a new entry?")
     os.makedirs(path)
     print("VIN not found in system, please input details about the vehicle")
     print("Please enter the Manufacturer of the Vehicle")
     if test_mode == True:
         make
+        print("TEST MODE")
     else:
         make = input()
     print("Please enter the Model")
     if test_mode == True:
         model
+        print("TEST MODE")
     else:
         model = input()
 
     print("Please enter the Year of Manufacture")
     if test_mode == True:
         year
+        print("TEST MODE")
     else:
         year = input()
 
     print("Please enter the Trim level")
     if test_mode == True:
         m_trim
+        print("TEST MODE")
     else:
         m_trim = input()
 
     print("Please enter the paint code")
     if test_mode == True:
         colour
+        print("TEST MODE")
     else:
         colour = input()
 
     print("Please enter the Engine specifications")
     if test_mode == True:
         engine
+        print("TEST MODE")
     else:
         engine = input()
 
     print("Please enter the Transmission type of the Vehicle")
     if test_mode == True:
         transmission
+        print("TEST MODE")
     else:
         transmission = input()
 
     print("Please enter the Chassis designation of the Vehicle")
     if test_mode == True:
         chassis
+        print("TEST MODE")
     else:
         chassis = input() #chassis code
     #head_info = {"VIN":vin, "Manufacturer":model, "Model": model, "Year": year, "Trim": m_trim, "Colour Code": colour, "Engine": engine, "Transmission": transmission, "Chassis Code": chassis}
@@ -81,14 +95,27 @@ if not os.path.exists(path):
     for i in head_info2:
         f.write(i + "," + head_info[n] + "\n")
         n += 1
-        print(n)
+        #print(n)
     headder = ["TIMESTAMP,","TIME_WORK_DONE,","TYPE_OF_WORK_DONE,","WORK_DONE_DESC,", "LOCATION_INFO","\n"]
     f.writelines(headder)
+    print("A new vehicle has been added! Please restart the program")
 
 else:
-    print("VIN Found, Please wait while maintainance records are being accessed")
-    f = open((path+vin+".csv"), 'a')
-
+    print("VIN Found, Please wait while maintainance records are being accessed \n")
+    f = open((path+vin+".csv"))
+    readF = csv.reader(f)
+    n = 0
+    for line in f.readlines():
+        if n < 9:
+            print(line.rstrip())
+            n += 1
+        else:
+            print("\nYour last service record as listed")
+            pass
+            last_line = line
+            print(last_line)
+            break
+        
 
 f.close()
 
