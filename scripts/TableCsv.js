ceed17fc3b4ee5766ed4055c31cd93d7db1eb012
keenanjude <60664206+keenanjude@users.noreply.github.com>
2021-10-10 21:50:42 -0400

    JS additions

---
+++ scripts/TableCsv.js
@@ -0,0 +1,10 @@
+export default class {
+  /**
+  *
+  * @param {HTMLTableElement} root the table element which will display the csv
+  */
+  constructor(root) {
+     this.root = root;
+     console.log("I am constructed");
+  }
+}
